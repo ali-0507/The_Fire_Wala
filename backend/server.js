d4dbff747495558d919846bb15a0c7a18a4e12bd
Shesh-21 <akshadashesh02@gmail.com>
2026-09-21 15:56:10 +0530

    neon connection

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ const pool = new Pool({
     port:Number(process.env.DB_PORT),
     database:process.env.DB_NAME,
     user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD
+    password:process.env.DB_PASSWORD,
+    ssl: { rejectUnauthorized: true }
 });
 
 //Testing DB connection
